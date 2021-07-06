278c69b9ce352cbd292b5d319099ea35c6148f7a
Brandon Goddard <brgoddar@microsoft.com>
2021-07-06 11:29:41 -0700

    fix css routing

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.static(__dirname + '/'));
+
 app.use(require('body-parser').json());
 
 /** @type Worker[] */
